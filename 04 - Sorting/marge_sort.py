f1c42d028167620a07abcb5ca9d08bc1638104b4
mridul <mridulroy010@gmail.com>
2026-03-13 23:27:15 +0600

    Quick Sort

--- 04 - Sorting/marge_sort.py
+++ 04 - Sorting/marge_sort.py
@@ -1,5 +1,7 @@
-left = [3,5,6]
-right = [2,3,5,6,7,9]
+# left = [3,5,6]
+# right = [2,3,5,6,7,9]
+
+arr = [3,7,1,5,8,4,3,9]
 
 def marge_arr(left,right):
     result = []
@@ -22,4 +24,16 @@ def marge_arr(left,right):
         j+=1
     return result
     
-print(marge_arr(left,right))
+# print(marge_arr(left,right))
+
+def marge_sort(arr):
+    if len(arr)<=1:
+        return arr
+    mid = len(arr)//2
+    left_arr = arr[ : mid]
+    right_arr = arr[mid : ]
+    left = marge_sort(left_arr)
+    right = marge_sort(right_arr)
+    return marge_arr(left,right)
+
+print(marge_sort(arr))
---
+++ 04 - Sorting/quick_sort.py
@@ -0,0 +1,26 @@
+nums = [2,4,6,1,9,4]
+
+n = len(nums)
+
+def partition(nums,low,high):
+    pivot = nums[low]
+    i=low
+    j=high
+    while i<j:
+        while nums[i]<=pivot and i<=high-1:
+            i+=1
+        while nums[j]>pivot and j>=low+1:
+            j-=1
+        if i<j:
+            nums[i],nums[j]=nums[j],nums[i]
+    nums[low],nums[j]=nums[j],nums[low]
+    return j
+
+def quick_sort(nums,low,high):
+    if low<high:
+        p_index = partition(nums,low,high)
+        quick_sort(nums,low,p_index-1)
+        quick_sort(nums,p_index+1,high)
+    
+    
+print(quick_sort(nums,0,7))
